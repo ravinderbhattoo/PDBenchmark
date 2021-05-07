537072d5179d5eeda87f9ccc6e2f2ec8a5dae8ce
Ravinder Bhattoo <ravinderbhattoo@gmail.com>
2021-05-07 07:18:23 +0530

    standard test added

--- src/tests/bar_tensile.jl
+++ src/tests/bar_tensile.jl
@@ -1,40 +1,29 @@
 export BarTensileQS
 
 function BarTensileQS(; gen_mat=nothing, spc_mat=nothing)
-    Steps = 10
-    solver = PDBenchmark.NameParam(:qs, (Steps, 1.0), Dict(:filewrite_freq=>1, :neigh_update_freq=>1, :file_prefix=>"minimize", :start_at=>0))
-    geom = PDBenchmark.NameParam(:Bar, (), Dict(:resolution=>0.5))
+    Steps = 6000
+    solver = PDBenchmark.NameParam(:vv, (Steps), Dict(:filewrite_freq=>100, :neigh_update_freq=>100, :file_prefix=>"BarTensileQS", :start_at=>0))
+    obj = () -> begin changetype(Cuboid([0 10; 0 2; 0 2]), out -> begin x=out[1]; x[1, :] .> 5.0 end, 2) end
+    geom = PDBenchmark.NameParam(obj, (), Dict(:resolution=>0.5))
 
-    if typeof(gen_mat) == Nothing
-        rho = 1000.0
-        horizon = 0.3
-        s = 0.5
-        gen_mat = PDBenchmark.NameParam(:GeneralMaterial, (rho, horizon, s), Dict())
-    end
+    
+    Es = 20
+    nu = 0.2
+    K = Es/3/(1-2nu)
+    G = Es/2/(1+nu)
+    rho = 1000.0
+    horizon = 2.0
+    s = 0.5
 
-    if typeof(spc_mat)==Nothing
-        Es = 20
-        nu = 0.2
-        K = Es/3/(1-2nu)
-        G = Es/2/(1+nu)
-        spc_mat = PDBenchmark.NameParam(:OrdinaryStateBasedSpecific, (K, G), Dict())
-    end
+    gen_mat = PDBenchmark.NameParam(:GeneralMaterial, (horizon), Dict(:max_neigh=>200))
+    
+    spc_mat = PDBenchmark.NameParam(:OrdinaryStateBasedSpecific, ([K, K, K], [G, G, G], [s, s, s], [rho, rho], ), Dict())
 
-    bc_f = out -> begin
-    x = out[1]
-    type = out[5][1]
-    scalemask_f = out -> vec(begin x=out[1]; x[1, :] .< Inf end)
-    fixmask_f = out -> vec(begin 
-                            x=out[1]; 
-                            mask1 = x[1,:] .<= 2.0 
-                            mask2 = x[1,:] .>= 8.0
-                            mask1 .| mask2  
-                        end)
-    return type, scalemask_f(out), [0.02, 0.0, 0.0], fixmask_f(out) 
-    end
+    bc_f1 = x -> (vec(x[1,:] .<= 2.0), )
+    bc_f2 = x -> (vec(x[1,:] .>= 8.0), [0.0005, 0, 0])
 
-    bc = PDBenchmark.NameParam(:ScaleFixBC, bc_f, Dict())
-    RM = PDBenchmark.NameParam(:SimpleRepulsionModel, (2.0, 1.0), Dict(:distanceX=>3, :max_neighs=>200))
+    bc = [PDBenchmark.NameParam(:FixBC, (bc_f1,)), PDBenchmark.NameParam(:MoveBC, (bc_f2,))]
+    RM = [PDBenchmark.NameParam(:SimpleRepulsionModel, (2.0, 100.0), Dict(:blocks=>(1,), :distanceX=>3, :max_neighs=>200))]
 
     f = env -> begin
         env.Params = Dict("left" => (env.y[1,:] .< 2))
