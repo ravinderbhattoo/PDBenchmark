9ee6772593b1fd8fdf07d2167f7cf1731f34ff80
Ravinder Bhattoo <ravinderbhattoo@gmail.com>
2021-05-07 07:17:53 +0530

    example added

--- examples/test.jl
+++ examples/test.jl
@@ -1,18 +1,7 @@
 using Pkg; Pkg.activate(".")
 import PDBenchmark
 
-rho = 1000.0
-horizon = 0.3
-s = 0.5
-gen_mat = PDBenchmark.NameParam(:GeneralMaterial, (rho, horizon, s), Dict())
-
-Es = 20
-nu = 0.2
-K = Es/3/(1-2nu)
-G = Es/2/(1+nu)
-spc_mat = PDBenchmark.NameParam(:OrdinaryStateBasedSpecific, (K, G), Dict())
-
-test = PDBenchmark.BarTensileQS(; gen_mat=gen_mat, spc_mat=spc_mat)
+test = PDBenchmark.BarTensileQS()
 env = PDBenchmark.run(test)
 
 y = env.Out["Force"][1, :]
