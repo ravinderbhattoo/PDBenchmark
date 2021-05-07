6dd05e67437462bd90975229aaf5c71ad0070461
Ravinder Bhattoo <ravinderbhattoo@gmail.com>
2021-05-07 07:18:11 +0530

    geom update

--- src/geom/bar.jl
+++ src/geom/bar.jl
@@ -12,7 +12,7 @@ function Bar(;bounds=[0.0 10; 0 2;0 2])
     Bar(Cuboid(bounds))
 end
 
-function create(geom::Bar; resolution=0.1, rand_=0.05, type=1)
+function PDMesh.create(geom::Bar; resolution=0.1, rand_=0.05, type=1)
     block = geom.obj
     return PDMesh.create(block; resolution=resolution, rand_=rand_, type=type)
 end
